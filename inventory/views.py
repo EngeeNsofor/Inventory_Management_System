d41953ebb2850443bd02874e97ac32820db0cd35
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-12-15 16:31:29 +0000

    Updates

--- inventory/views.py
+++ inventory/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import InventoryItem
 from .serializers import InventoryItemSerializer, UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated 
 
 # Create your views here.
 
@@ -10,6 +11,7 @@ class InventoryItemViewSet(viewsets.ModelViewSet):
 
     queryset = InventoryItem.objects.all()
     serializer_class = InventoryItemSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserViewSet(viewsets.ModelViewSet):
