94876189009c0334972e52869b4fee6c717b68c8
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-12-15 15:13:16 +0000

    Implemented inventory item CRUD functionality

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import InventoryItem
+from .serializers import InventoryItemSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+class InventoryItemViewSet(viewsets.ModelViewSet):
+    """
+    viewsets.ModelViewSet provides a set of default actions 
+    like create(), list(), retrieve(), update(), and destroy() 
+    without needing to write them explicitly.
+    """
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
